a22a2f6798a44e3f30cf1d0b2097415b5ef045ea
dracoIntelligentSystem <draco_@hotmail.it>
2015-03-18 14:15:09 +0100

    Update work

--- src/myImage.java
+++ src/myImage.java
@@ -7,16 +7,18 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
+import sun.font.StrikeCache;
 
 
 
@@ -27,50 +29,83 @@ class myImage extends JFrame  implements ActionListener
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	ImageIcon m[];
-	JLabel l;
-	JButton b,b1;
-	int i,l1,size;
-	static int IMG_WIDTH=321;
-	static int IMG_HEIGHT=470;
-	JPanel p;
-	String path = "C:\\Copy\\eclipse\\workspace\\ImageSlide\\immagini\\";
-	HashMap<String, ArrayList<ImageIcon>> immagini = new HashMap<String, ArrayList<ImageIcon>>();
+	private ImageIcon img[];
+	private String imgPath[];
+	private JLabel l, infoImg;
+	private JButton prev,next, confirm, back;
+	private int i,l1,size;
+	private static int IMG_WIDTH=321;
+	private static int IMG_HEIGHT=470;
+	private JPanel Principal,slider,button;
+	private String path = "C:\\Users\\dragoSI\\git\\ImageSlider\\immagini\\", emoFolder;
+	private HashMap<String, HashMap<File, ImageIcon>> immagini = new HashMap<String, HashMap<File, ImageIcon>>();
 
-	public myImage() throws IOException
+
+	public myImage(String emo) throws IOException
 	{
-		
-		immagini.put("test", new ArrayList<ImageIcon>());
-		resizeImage(path);
+		this.emoFolder=emo;
+		immagini.put("test", new HashMap<File, ImageIcon>());
+		resizeImage(path+emo+"\\");
 		size=immagini.get("test").size();
+		i=size;
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLayout(new BorderLayout( ));
+		//setLayout(new BorderLayout( ));
 		setVisible(true);
-		setSize(500, 500);		
-		JPanel p=new JPanel(new FlowLayout());
-		b=new JButton("Pre");
-		b1=new JButton("Next");
-		p.add(b);
-		p.add(b1);
-		add(p,BorderLayout.SOUTH);
-		b.addActionListener(this);
-		b1.addActionListener(this);
-		m = new ImageIcon[size];
-		int count=0;
-		for(ImageIcon icon : immagini.get("test"))
+		//setSize(500, 500);		
+		
+		//JPanel p=new JPanel(new FlowLayout());
+		Principal=new JPanel();		
+		Principal.setLayout(new BoxLayout(Principal, BoxLayout.Y_AXIS));
 		{
-			m[count] = icon;
+			slider= new JPanel(new FlowLayout());
+
+			prev=new JButton("<");
+			prev.setSize(prev.getWidth(), 471);
+			slider.add(prev);
+
+			l = new JLabel();
+			slider.add(l);
+
+			next=new JButton(">");
+			next.setSize(next.getWidth(), 471);
+			slider.add(next);
+		}
+		img = new ImageIcon[size];
+		imgPath = new String[size];
+		int count=0;
+		for(File fileOriginalImage: immagini.get("test").keySet())
+		{
+			img[count] = immagini.get("test").get(fileOriginalImage);
+			imgPath[count] = fileOriginalImage.getAbsolutePath();
 			count++;
 		}
-//		m = new ImageIcon[2];
-//		m[0] = new ImageIcon(path+"disgusto1.png");
-//
-//		m[1] = new ImageIcon(path+"disgusto2.png");
-		l = new JLabel();
-		l.setBounds(400, 0, getWidth(), getHeight());
-		add(l,BorderLayout.CENTER);
-		l.setIcon(m[0]);
-		//this.pack();
+		
+		
+		JPanel infoPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		System.out.println(imgPath[0]);
+		System.out.println(((imgPath[0].substring(imgPath[0].lastIndexOf("\\")+1))));
+		infoImg = new JLabel((imgPath[0].substring(imgPath[0].lastIndexOf("\\")+1)).split("\\.")[0]);
+		l.setIcon(img[0]);
+		infoPanel.add(infoImg);
+		
+		{
+			button = new JPanel(new FlowLayout());
+			confirm = new JButton("CONFIRM");
+			back = new JButton("BACK");
+			button.add(confirm);
+			button.add(back);
+		}
+		
+		prev.addActionListener(this);
+		next.addActionListener(this);
+		confirm.addActionListener(this);
+		back.addActionListener(this);
+		Principal.add(slider);
+		Principal.add(infoPanel);
+		Principal.add(button);
+		this.getContentPane().add(Principal);
+		this.pack();
+		this.setResizable(false);
 	}
 
 	//riarrangia dimensioni a quelle della finestra
@@ -83,7 +118,7 @@ class myImage extends JFrame  implements ActionListener
 			Graphics2D g = resizedImage.createGraphics();
 			g.drawImage(originalImage, 0, 0, IMG_WIDTH, IMG_HEIGHT, null);
 			g.dispose();
-			immagini.get("test").add(new ImageIcon(resizedImage));
+			immagini.get("test").put(fileImage, new ImageIcon(resizedImage));
 			//String nomeEmo = fileImage.getName();
 			
 		}
@@ -92,37 +127,27 @@ class myImage extends JFrame  implements ActionListener
 	public  void actionPerformed(ActionEvent e)
 	{
 
-		if(e.getSource()==b)
+		if(e.getSource()==prev)
 		{
-			if(i==0)
-			{
-				JOptionPane.showMessageDialog(null,"This is First Image");
-			}
-			else
-			{
-				i=(i-1) % size;
-				l.setIcon(m[i]);
-			}
+			i=(i-1) % size;
+			l.setIcon(img[i]);
 		}
-		if(e.getSource()==b1)
-		{
-			if(i==m.length-1)
-			{
-				JOptionPane.showMessageDialog(null,"This is LastImage");
-			}
-			else
-			{
-				i=(i+1)% size;
-				l.setIcon(m[i]);
-			}
+		if(e.getSource()==next)
+		{	
+			i=(i+1)% size;
+			l.setIcon(img[i]);
 		}
-
+		infoImg.setText((imgPath[i].substring(imgPath[i].lastIndexOf("\\")+1)).split("\\.")[0]);
+		l.setIcon(img[i]);
 	}
 
 
 	public static void main(String args[]) throws IOException
 	{
-		myImage i1 = new myImage();
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Inserisci l'emozione da ricercare: ");
+		String emo = sc.nextLine();
+		myImage i1 = new myImage(emo);
 	}
 
 
